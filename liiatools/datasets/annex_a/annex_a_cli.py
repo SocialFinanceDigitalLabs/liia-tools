051663fa0611f77d6b8c40ed5930c3174832f2f2
MichaelHanksSF <72440606+MichaelHanksSF@users.noreply.github.com>
2022-06-09 16:16:46 +0100

    la_gg minus testing

--- liiatools/datasets/annex_a/annex_a_cli.py
+++ liiatools/datasets/annex_a/annex_a_cli.py
@@ -3,14 +3,16 @@ from pathlib import Path
 import yaml
 
 from sfdata_stream_parser.parser import openpyxl
+from liiatools.datasets.annex_a.lds_annexa_clean import configuration as clean_config
 from liiatools.datasets.annex_a.lds_annexa_clean import (
-    configuration,
     cleaner,
     degrade,
     logger,
     populate,
     file_creator,
 )
+from liiatools.datasets.annex_a.lds_annexa_la_agg import configuration as agg_config
+from liiatools.datasets.annex_a.lds_annexa_la_agg import process
 from liiatools.spec import common as common_asset_dir
 from liiatools.datasets.shared_functions.common import flip_dict
 from sfdata_stream_parser.filters.column_headers import promote_first_row
@@ -57,7 +59,7 @@ def cleanfile(input, la_code, la_log_dir, output):
     """
     Cleans input Annex A xlsx files according to config and outputs cleaned xlsx files.
     :param input: should specify the input file location, including file name and suffix, and be usable by a Path function
-    :param la_code: should be a string of the name of the local authority that deposited the file
+    :param la_code: should be a three-letter string for the local authority depositing the file
     :param la_log_dir: should specify the path to the local authority's log folder
     :param output: should specify the path to the output folder
     :return: None
@@ -65,7 +67,7 @@ def cleanfile(input, la_code, la_log_dir, output):
 
     # Configuration
     filename = Path(input).resolve().stem
-    config = configuration.Config()
+    config = clean_config.Config()
     la_name = flip_dict(config["data_codes"])[la_code]
 
     # Open & Parse file
@@ -74,7 +76,7 @@ def cleanfile(input, la_code, la_log_dir, output):
     stream = promote_first_row(stream)
 
     # Configure Stream
-    stream = configuration.configure_stream(stream, config)
+    stream = clean_config.configure_stream(stream, config)
 
     # Clean stream
     stream = cleaner.clean(stream)
@@ -83,5 +85,51 @@ def cleanfile(input, la_code, la_log_dir, output):
     stream = populate.create_la_child_id(stream, la_code=la_code)
 
     # Output result
-    stream = file_creator.save_stream(stream, la_name, la_log_dir, output)
+    stream = file_creator.save_stream(stream, la_name, output)
     stream = logger.save_errors_la(stream, la_log_dir=la_log_dir)
+
+
+@annex_a.command()
+@click.option(
+    "--i",
+    "input",
+    required=True,
+    type=str,
+    help="A string specifying the input file location, including the file name and suffix, usable by a pathlib Path function",
+)
+@click.option(
+    "--o",
+    "output",
+    required=True,
+    type=str,
+    help="A string specifying the output directory location",
+)
+def la_agg(input, output):
+    '''
+    Joins data from newly cleaned Annex A file (output of cleanfile()) to existing Annex A data for the depositing local authority
+    :param input: a string specifying the input file location of the newly cleaned file, including file name and suffix, usable by a Path function
+    :param output: a string specifying the local authority's Annex A output directory, usable by a Path function
+    :return: None
+    '''
+
+    # Configuration
+    config = agg_config.Config()
+
+    # Open cleaned file as dictionary
+    aa_dict = process.split_file(input)
+
+    # Merge with existing LA files, if any
+    list_columns = config["sort_order"]
+    aa_dict = process.sort_dict(aa_dict, list_columns)
+    aa_dict = process.merge_la_files(output, aa_dict)
+
+    # Remove duplicate data and data older than retention period
+    dedup = config["dedup"]
+    dates = config["dates"]
+    index_date = config["index_date"]
+    aa_dict = process.deduplicate(aa_dict, dedup)
+    aa_dict = process.convert_dates(aa_dict, dates)
+    aa_dict = process.remove_old_data(aa_dict, index_date)
+
+    # Output result
+    process.export_file(output, aa_dict)
