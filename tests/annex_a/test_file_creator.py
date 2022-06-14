b6b3e657132649bced06c0ec1716ba98b47e5580
patrick-troy <58770937+patrick-troy@users.noreply.github.com>
2022-06-14 10:59:31 +0100

    Update test_file_creator.py

--- tests/annex_a/test_file_creator.py
+++ tests/annex_a/test_file_creator.py
@@ -1,20 +1,21 @@
 import tablib
+import tempfile as tmp
+from unittest.mock import patch, mock_open
+import os
 
 from sfdata_stream_parser import events
 
-from liiatools.datasets.annex_a.lds_annexa_clean import (
-    file_creator
-)
+from liiatools.datasets.annex_a.lds_annexa_clean import file_creator
 
 
 def test_save_tables():
     data = tablib.Dataset(headers=["Name", "Age"])
     data.append(("Kenneth", 12))
+    output = tmp.gettempdir()
 
     stream = file_creator.save_tables(
         [
             events.StartContainer(),
-
             events.StartTable(sheet_name="List 1"),
             file_creator.TableEvent(data=data),
             events.StartTable(sheet_name="List 2"),
@@ -37,10 +38,17 @@ def test_save_tables():
             file_creator.TableEvent(data=data),
             events.StartTable(sheet_name="List 11"),
             file_creator.TableEvent(data=data),
-
             events.EndContainer(filename="test"),
-
         ],
-        "Output"
+        output,
     )
-
+    stream = list(stream)
+    if isinstance(stream, file_creator.TableEvent):
+        assert stream.data is not None
+    if isinstance(stream, events.EndContainer):
+        open_mock = mock_open()
+        with patch("builtins.open", open_mock, create=True):
+            file_creator.save_tables("test-data", output)
+        open_mock.assert_called_with(
+            f"{os.path.join(output, stream.filename)}_clean.xlsx", "wb"
+        )
