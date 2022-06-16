5e4a1d0195fe9ab75d2bed3ebb16fb3fc70e4568
MichaelHanksSF <72440606+MichaelHanksSF@users.noreply.github.com>
2022-06-16 12:05:32 +0100

    Removed use of dict keyword

--- liiatools/datasets/annex_a/lds_annexa_la_agg/process.py
+++ liiatools/datasets/annex_a/lds_annexa_la_agg/process.py
@@ -12,47 +12,47 @@ def split_file(input):
     return dict
 
 
-def sort_dict(dict, sort_order):
+def sort_dict(aa_dict, sort_order):
     '''Sorts the imported dict by List to ensure consistency'''
-    for k in dict.keys():
-        df = dict[k]
+    for k in aa_dict.keys():
+        df = aa_dict[k]
         df = df[sort_order[k]]
-        dict[k] = df
-    return dict
+        aa_dict[k] = df
+    return aa_dict
 
 
-def _merge_dfs(new_dict, old_dict):
-    for k in new_dict.keys():
-        new_df = new_dict[k]
+def _merge_dfs(aa_dict, old_dict):
+    for k in aa_dict.keys():
+        new_df = aa_dict[k]
         old_df = old_dict[k]
         merged_df = pd.concat([new_df, old_df], axis=0, ignore_index=True)
-        new_dict[k] = merged_df
-    return new_dict
+        aa_dict[k] = merged_df
+    return aa_dict
 
 
-def merge_la_files(output, new_dict):
+def merge_la_files(output, aa_dict):
     file_list = Path(output).glob('*.xlsx')
     for file in file_list:
         old_dict = pd.read_excel(file, sheet_name=None, index_col=None, dtype=object)
-        _merge_dfs(new_dict, old_dict)
-    return new_dict
+        _merge_dfs(aa_dict, old_dict)
+    return aa_dict
 
 
-def deduplicate(dict, dedup):
-    for key in dict.keys():
-        df = dict[key]
+def deduplicate(aa_dict, dedup):
+    for key in aa_dict.keys():
+        df = aa_dict[key]
         df = df.drop_duplicates(subset=dedup[key], keep = 'first')
-        dict[key] = df
-    return dict
+        aa_dict[key] = df
+    return aa_dict
 
 
-def convert_datetimes(dict, dates):
-    for key in dict.keys():
-        df = dict[key]
+def convert_datetimes(aa_dict, dates):
+    for key in aa_dict.keys():
+        df = aa_dict[key]
         for date_field in dates[key]:
             df[date_field] = pd.to_datetime(df[date_field], format="%d/%m/%Y")
-        dict[key] = df
-    return dict
+        aa_dict[key] = df
+    return aa_dict
 
 
 def _remove_years(d, years):
@@ -62,34 +62,34 @@ def _remove_years(d, years):
         return d + (date(d.year - years, 1, 1) - date(d.year, 1, 1))
 
 
-def remove_old_data(dict, index_date):
+def remove_old_data(aa_dict, index_date):
     today = pd.to_datetime('today')
     six_years_ago = _remove_years(today, 6)
     thirty_years_ago = _remove_years(today, 30)
-    for key in dict.keys():
-        df = dict[key]
+    for key in aa_dict.keys():
+        df = aa_dict[key]
         if key == 'List 9':
             df = df[df[index_date[key]] >= thirty_years_ago]
         elif key == 'List 10':
             df = df[(df[index_date[key]] >= six_years_ago).any(axis=1)]
         else:
             df = df[(df[index_date[key]] >= six_years_ago) | (df[index_date[key]].isnull())]
-        dict[key] = df
-    return dict
+        aa_dict[key] = df
+    return aa_dict
 
 
-def convert_dates(dict, dates):
-    for key in dict.keys():
-        df = dict[key]
+def convert_dates(aa_dict, dates):
+    for key in aa_dict.keys():
+        df = aa_dict[key]
         for date_field in dates[key]:
             df[date_field] = pd.to_datetime(df[date_field], format="%d/%m/%Y").dt.date
-        dict[key] = df
-    return dict
+        aa_dict[key] = df
+    return aa_dict
 
 
-def export_file(output, dict):
+def export_file(output, aa_dict):
     output_path = Path(output, f"AnnexA_merged.xlsx")
     with pd.ExcelWriter(output_path) as writer:
-        for k in dict.keys():
-            df = dict[k]
+        for k in aa_dict.keys():
+            df = aa_dict[k]
             df.to_excel(writer, sheet_name=k, index=False)
