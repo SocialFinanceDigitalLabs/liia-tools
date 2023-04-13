e136893cf551d4e1a73c43b48deaa889c1d8658f
patrick-troy <58770937+patrick-troy@users.noreply.github.com>
2023-04-13 12:58:29 +0100

    update to allow for text after file_version_12_18/19 style file names

--- liiatools/datasets/shared_functions/common.py
+++ liiatools/datasets/shared_functions/common.py
@@ -129,7 +129,7 @@ def check_year(filename):
             year = match.group(1) + match.group(2)
             return year
 
-    fy_match = re.search(r"(\d{2})(.{0,3}\d{2}).*(\d*)", filename)
+    fy_match = re.search(r"(\d{2})(.{0,3}\d{2})(.*)(\d*)", filename)
     if fy_match:
         if len(fy_match.group(2)) == 2 and int(fy_match.group(2)) == int(fy_match.group(1)) + 1:
             year = "20" + fy_match.group(2)
@@ -137,8 +137,11 @@ def check_year(filename):
         if len(fy_match.group(2)) == 3 and int(fy_match.group(2)[-2:]) == int(fy_match.group(1)) + 1:
             year = "20" + fy_match.group(2)[-2:]
             return year
-        if int(fy_match.group(0)[-2:]) == int(fy_match.group(2)[-2:]) + 1:
-            year = "20" + fy_match.group(0)[-2:]
+        if int(fy_match.group(3)[1:3]) == int(fy_match.group(2)[-2:]) + 1:
+            year = "20" + fy_match.group(3)[1:3]
+            return year
+        if int(fy_match.group(2)[-2:]) == int(fy_match.group(2)[-4:-2]) + 1:
+            year = "20" + fy_match.group(2)[-2:]
             return year
         else:
             raise AttributeError
--- tests/common/test_common.py
+++ tests/common/test_common.py
@@ -59,6 +59,8 @@ def test_check_year():
     assert check_year("file_1920") == "2020"
     assert check_year("file_21/22") == "2022"
     assert check_year("file_version_12_18/19") == "2019"
+    assert check_year("file_version_1_18/19_final") == "2019"
+    assert check_year("file_version_1_1819") == "2019"
 
 
 class TestCheckYear(unittest.TestCase):
