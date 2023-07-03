e3d673c3406376b4f994716ec535b3947b14c119
YeeAuLC <116083089+YeeAuLC@users.noreply.github.com>
2023-07-03 12:59:14 +0000

    WIP 1

--- liiatools/datasets/social_work_workforce/csww_main_functions.py
+++ liiatools/datasets/social_work_workforce/csww_main_functions.py
@@ -1,3 +1,5 @@
+import logging
+import click_log
 from liiatools.datasets.social_work_workforce.sample_data import (
     generate_sample_csww_file,
 )
@@ -25,3 +27,49 @@ def generate_sample(output: str):
             FILE.write(element)
     except FileNotFoundError:
         print("The file path provided does not exist")
+
+
+def cleanfile(input, la_code, la_log_dir, output):
+    """
+    Cleans input CIN Census xml files according to config and outputs cleaned csv files.
+    :param input: should specify the input file location, including file name and suffix, and be usable by a Path function
+    :param la_code: should be a three-letter string for the local authority depositing the file
+    :param la_log_dir: should specify the path to the local authority's log folder
+    :param output: should specify the path to the output folder
+    :return: None
+    """
+
+    # Open & Parse file
+    if (
+        check_file_type(
+            input,
+            file_types=[".xml"],
+            supported_file_types=supported_file_types,
+            la_log_dir=la_log_dir,
+        )
+        == "incorrect file type"
+    ):
+        return
+    stream = dom_parse(input)
+    stream = list(stream)
+
+    # Get year from input file
+    try:
+        filename = str(Path(input).resolve().stem)
+        input_year = check_year(filename)
+    except (AttributeError, ValueError):
+        save_year_error(input, la_log_dir)
+        return
+
+    # Check year is within acceptable range for data retention policy
+    years_to_go_back = 6
+    year_start_month = 6
+    reference_date = datetime.now()
+    if (
+        check_year_within_range(
+            input_year, years_to_go_back, year_start_month, reference_date
+        )
+        is False
+    ):
+        save_incorrect_year_error(input, la_log_dir)
+        return
