0d72e3b0e5a47f8f4453ff9c5f2e29b7a94bca93
YeeAuLC <116083089+YeeAuLC@users.noreply.github.com>
2023-07-05 15:26:28 +0000

    wip 20230705 7

--- liiatools/datasets/social_work_workforce/csww_cli.py
+++ liiatools/datasets/social_work_workforce/csww_cli.py
@@ -1,9 +1,21 @@
 import logging
 import click as click
 import click_log
+from pathlib import Path
+import yaml
+from datetime import datetime
 
 from liiatools.datasets.social_work_workforce import csww_main_functions
 
+from liiatools.spec import common as common_asset_dir
+
+log = logging.getLogger()
+click_log.basic_config(log)
+
+COMMON_CONFIG_DIR = Path(common_asset_dir.__file__).parent
+# Get all the possible LA codes that could be used
+with open(f"{COMMON_CONFIG_DIR}/LA-codes.yml") as las:
+    la_list = list(yaml.full_load(las)["data_codes"].values())
 
 logger = logging.getLogger()
 click_log.basic_config(logger)
@@ -37,10 +49,31 @@ def generate_sample(output: str):
     return output
 
 # added to run from command line
+@csww.command()
+@click.option(
+    "--i",
+    "input",
+    required=True,
+    type=str,
+    help="A string specifying the input file location, including the file name and suffix, usable by a pathlib Path function",
+)
+@click.option(
+    "--la_log_dir",
+    required=True,
+    type=str,
+    help="A string specifying the location that the log files for the LA should be output, usable by a pathlib Path function.",
+)
+@click.option(
+    "--o",
+    "output",
+    required=True,
+    type=str,
+    help="A string specifying the output directory location",
+)
 @click_log.simple_verbosity_option(log)
 def cleanfile(input, la_code, la_log_dir, output):
     """
-    Cleans input social_work_workforce xml files according to config and outputs cleaned csv files.
+    Cleans input CIN Census xml files according to config and outputs cleaned csv files.
     :param input: should specify the input file location, including file name and suffix, and be usable by a Path function
     :param la_code: should be a three-letter string for the local authority depositing the file
     :param la_log_dir: should specify the path to the local authority's log folder
