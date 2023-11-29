6866cd08b8331f86a69307f0f0f320debfeede69
patrick-troy <58770937+patrick-troy@users.noreply.github.com>
2023-11-29 13:38:25 +0000

    adjust unit test

--- tests/social_work_workforce/test_stream_pipeline.py
+++ tests/social_work_workforce/test_stream_pipeline.py
@@ -1,31 +1,29 @@
 from fs import open_fs
 
-from liiatools.cin_census_pipeline.spec import load_schema
-from liiatools.cin_census_pipeline.spec.samples import CIN_2022
-from liiatools.cin_census_pipeline.spec.samples import DIR as SAMPLES_DIR
-from liiatools.cin_census_pipeline.stream_pipeline import task_cleanfile
+from liiatools.csww_pipeline.spec import load_schema, load_schema_path
+from liiatools.csww_pipeline.spec.samples import CSWW_2022
+from liiatools.csww_pipeline.spec.samples import DIR as SAMPLES_DIR
+from liiatools.csww_pipeline.stream_pipeline import task_cleanfile
 from liiatools.common.data import FileLocator
 
 
 def test_task_cleanfile():
     samples_fs = open_fs(SAMPLES_DIR.as_posix())
-    locator = FileLocator(samples_fs, CIN_2022.name)
+    locator = FileLocator(samples_fs, CSWW_2022.name)
 
-    data = task_cleanfile(locator, schema=load_schema(2022))
+    result = task_cleanfile(locator, schema=load_schema(2022), schema_path=load_schema_path(2022))
 
-    assert len(data) == 10
-    assert len(data.headers) == 34
+    data = result.data
+    errors = result.errors
 
+    assert len(data) == 3
+    assert len(data["Worker"]) == 1
+    assert len(data["Worker"].columns) == 23
+    assert len(data["LA_Level"]) == 1
+    assert len(data["LA_Level"].columns) == 3
 
-if __name__ == "__main__":
-    from pathlib import Path
-
-    samples_fs = open_fs(SAMPLES_DIR.as_posix())
-    locator = FileLocator(samples_fs, CIN_2022.name)
-
-    data = task_cleanfile(locator, schema=load_schema(2022))
-
-    build_dir = Path(__file__).parent.parent.parent / "build"
-    build_dir.mkdir(exist_ok=True)
-
-    (build_dir / "cin_census_2022_flatfile.csv").write_text(data.export("csv"))
+    assert errors[0]["type"] == "ConversionError"
+    assert errors[0]["message"] == "Could not convert to date"
+    assert errors[0]["exception"] == "Invalid date: 2023-03-28T14:54:55Z"
+    assert errors[0]["filename"] == "social_work_workforce_2022.xml"
+    assert errors[0]["header"] == "DateTime"
