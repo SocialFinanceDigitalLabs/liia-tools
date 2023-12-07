7891b0f16cfa485951fc8490f29594b910234fc4
patrick-troy <58770937+patrick-troy@users.noreply.github.com>
2023-12-07 13:52:05 +0000

    fix test_parse

--- tests/social_work_workforce/test_parse.py
+++ tests/social_work_workforce/test_parse.py
@@ -1,4 +1,4 @@
-from io import StringIO, BytesIO
+from io import BytesIO
 
 from liiatools.csww_pipeline.stream_parse import dom_parse
 from sfdata_stream_parser.events import (
@@ -24,29 +24,91 @@ def test_dom_parse():
 
     events = [(type(event), event.as_dict()) for event in stream]
 
-    assert events == [
-        (StartElement, {"attrib": {}, "filename": "csww.xml", "tag": "a"}),
-        (TextNode, {"cell": None, "filename": "csww.xml", "text": None}),
-        (
-            ProcessingInstructionNode,
-            {
-                "cell": "PIContent",
-                "filename": "csww.xml",
-                "name": "PITarget",
-                "text": None,
-            },
-        ),
-        (StartElement, {"attrib": {}, "filename": "csww.xml", "tag": "b"}),
-        (TextNode, {"cell": "1", "filename": "csww.xml", "text": None}),
-        (StartElement, {"attrib": {"frog": "f"}, "filename": "csww.xml", "tag": "c"}),
-        (TextNode, {"cell": "2a", "filename": "csww.xml", "text": None}),
-        (CommentNode, {"cell": " yeah ", "filename": "csww.xml", "text": None}),
-        (StartElement, {"attrib": {}, "filename": "csww.xml", "tag": "d"}),
-        (TextNode, {"cell": None, "filename": "csww.xml", "text": None}),
-        (EndElement, {"filename": "csww.xml", "tag": "d"}),
-        (TextNode, {"cell": "3", "filename": "csww.xml", "text": None}),
-        (EndElement, {"filename": "csww.xml", "tag": "c"}),
-        (EndElement, {"filename": "csww.xml", "tag": "b"}),
-        (TextNode, {"cell": "4", "filename": "csww.xml", "text": None}),
-        (EndElement, {"filename": "csww.xml", "tag": "a"}),
-    ]
+    assert events[0][0] == StartElement
+    assert (
+        events[0][1].items()
+        >= {"attrib": {}, "filename": "csww.xml", "tag": "a"}.items()
+    )
+
+    assert events[1][0] == TextNode
+    assert (
+        events[1][1].items()
+        >= {"cell": None, "filename": "csww.xml", "text": None}.items()
+    )
+
+    assert events[2][0] == ProcessingInstructionNode
+    assert (
+        events[2][1].items()
+        >= {
+            "cell": "PIContent",
+            "filename": "csww.xml",
+            "name": "PITarget",
+            "text": None,
+        }.items()
+    )
+
+    assert events[3][0] == StartElement
+    assert (
+        events[3][1].items()
+        >= {"attrib": {}, "filename": "csww.xml", "tag": "b"}.items()
+    )
+
+    assert events[4][0] == TextNode
+    assert (
+        events[4][1].items()
+        >= {"cell": "1", "filename": "csww.xml", "text": None}.items()
+    )
+
+    assert events[5][0] == StartElement
+    assert (
+        events[5][1].items()
+        >= {"attrib": {"frog": "f"}, "filename": "csww.xml", "tag": "c"}.items()
+    )
+
+    assert events[6][0] == TextNode
+    assert (
+        events[6][1].items()
+        >= {"cell": "2a", "filename": "csww.xml", "text": None}.items()
+    )
+
+    assert events[7][0] == CommentNode
+    assert (
+        events[7][1].items()
+        >= {"cell": " yeah ", "filename": "csww.xml", "text": None}.items()
+    )
+
+    assert events[8][0] == StartElement
+    assert (
+        events[8][1].items()
+        >= {"attrib": {}, "filename": "csww.xml", "tag": "d"}.items()
+    )
+
+    assert events[9][0] == TextNode
+    assert (
+        events[9][1].items()
+        >= {"cell": None, "filename": "csww.xml", "text": None}.items()
+    )
+
+    assert events[10][0] == EndElement
+    assert events[10][1].items() >= {"filename": "csww.xml", "tag": "d"}.items()
+
+    assert events[11][0] == TextNode
+    assert (
+        events[11][1].items()
+        >= {"cell": "3", "filename": "csww.xml", "text": None}.items()
+    )
+
+    assert events[12][0] == EndElement
+    assert events[12][1].items() >= {"filename": "csww.xml", "tag": "c"}.items()
+
+    assert events[13][0] == EndElement
+    assert events[13][1].items() >= {"filename": "csww.xml", "tag": "b"}.items()
+
+    assert events[14][0] == TextNode
+    assert (
+        events[14][1].items()
+        >= {"cell": "4", "filename": "csww.xml", "text": None}.items()
+    )
+
+    assert events[15][0] == EndElement
+    assert events[15][1].items() >= {"filename": "csww.xml", "tag": "a"}.items()
