ae959f599f0816080195df0c76c23eebce79f35d
patrick-troy <58770937+patrick-troy@users.noreply.github.com>
2024-02-12 15:37:36 +0000

    fix empty int error

--- liiatools/common/stream_pipeline.py
+++ liiatools/common/stream_pipeline.py
@@ -1,5 +1,6 @@
 from typing import Dict, List
 import pandas as pd
+import xml.etree.ElementTree as ET
 
 from liiatools.common.spec.__data_schema import Column
 
@@ -13,4 +14,31 @@ def to_dataframe(data: List[Dict], table_config: Dict[str, Column]) -> pd.DataFr
         elif column_spec.type == "category":
             # set type to categorical
             df[column_name] = df[column_name].astype("category")
+        elif column_spec.type == "numeric":
+            if column_spec.numeric.type == "integer":
+                # set type to Int64
+                df[column_name] = pd.to_numeric(df[column_name], errors="raise").astype('Int64')
+    return df
+
+
+def to_dataframe_xml(data: List[Dict], table_config) -> pd.DataFrame:
+    df = pd.DataFrame(data)
+    xsd_xml = ET.parse(table_config)
+
+    for column_name in df.columns:
+        search_elem = f".//{{http://www.w3.org/2001/XMLSchema}}element[@name='{column_name}']"
+        element = xsd_xml.find(search_elem)
+
+        if element is not None:
+            column_type = element.attrib.get("type", None)
+            if column_type is not None:
+                if column_type == "xs:date":
+                    # Set dtype on date columns
+                    df[column_name] = pd.to_datetime(df[column_name], errors="raise").dt.date
+                elif column_type == "positiveintegertype":
+                    # set type to Int64
+                    df[column_name] = pd.to_numeric(df[column_name], errors="raise").astype('Int64')
+                elif column_type[-4:] == "type":
+                    # set type to categorical
+                    df[column_name] = df[column_name].astype("category")
     return df
