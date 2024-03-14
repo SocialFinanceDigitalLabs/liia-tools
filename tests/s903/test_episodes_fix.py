cd1472edb55d9c3ef90d7ee525e4fe5fa3991473
Stephen C <127780498+StephenCarterLIIA@users.noreply.github.com>
2024-03-14 12:47:45 +0000

    Add unit test

--- tests/s903/test_episodes_fix.py
+++ tests/s903/test_episodes_fix.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 from liiatools.datasets.s903.lds_ssda903_episodes_fix.process import (
     create_previous_and_next_episode,
+    add_latest_year_and_source_for_la,
 )
 
 
@@ -18,9 +19,45 @@ def test_create_previous_and_next_episode():
     columns = ["DECOM", "RNE", "YEAR"]
 
     data_with_previous_next_episode = create_previous_and_next_episode(data, columns)
-    assert data_with_previous_next_episode["DECOM_previous"].tolist() == [None, "2016-07-26", "2016-08-22"]
-    assert data_with_previous_next_episode["DECOM_next"].tolist() == ["2016-08-22", "2016-09-13", None]
+    assert data_with_previous_next_episode["DECOM_previous"].tolist() == [
+        None,
+        "2016-07-26",
+        "2016-08-22",
+    ]
+    assert data_with_previous_next_episode["DECOM_next"].tolist() == [
+        "2016-08-22",
+        "2016-09-13",
+        None,
+    ]
     assert data_with_previous_next_episode["RNE_previous"].tolist() == [None, "S", "L"]
     assert data_with_previous_next_episode["RNE_next"].tolist() == ["L", "P", None]
-    assert data_with_previous_next_episode["YEAR_previous"].tolist() == [None, 2016, 2016]
+    assert data_with_previous_next_episode["YEAR_previous"].tolist() == [
+        None,
+        2016,
+        2016,
+    ]
     assert data_with_previous_next_episode["YEAR_next"].tolist() == [2016, 2016, None]
+
+
+def test_add_latest_year_and_source_for_la():
+    data = pd.DataFrame(
+        {
+            "LA": ["BAD", "BAD", "NEW", "NEW"],
+            "YEAR": [2019, 2020, 2022, 2021],
+        }
+    )
+
+    data_with_latest_year_and_source_for_la = add_latest_year_and_source_for_la(data)
+    assert data_with_latest_year_and_source_for_la["YEAR_latest"].tolist() == [
+        2020,
+        2020,
+        2022,
+        2022,
+    ]
+    assert data_with_latest_year_and_source_for_la["Episode_source"].tolist() == [
+        "Original",
+        "Original",
+        "Original",
+        "Original",
+    ]
+
