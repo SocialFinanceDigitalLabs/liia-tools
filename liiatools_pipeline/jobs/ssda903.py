89a60e347bb67ceb02c13d6ad4af31bed49e1a8a
patrick-troy <58770937+patrick-troy@users.noreply.github.com>
2024-03-21 09:29:10 +0000

    add error reporting

--- liiatools_pipeline/jobs/ssda903.py
+++ liiatools_pipeline/jobs/ssda903.py
@@ -5,10 +5,10 @@ from liiatools_pipeline.ops import ssda903
 
 @job
 def ssda903_incoming():
-    session_folder, incoming_files = ssda903.create_session_folder()
+    session_folder, session_id, incoming_files = ssda903.create_session_folder()
     archive = ssda903.open_archive()
 
-    processed = ssda903.process_files(session_folder, incoming_files, archive)
+    processed = ssda903.process_files(session_folder, incoming_files, archive, session_id)
 
     current_data = ssda903.create_current_view(archive, start=processed)
 
