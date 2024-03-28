4a48b5c22cae6fc82e6ca8fecd6e59aa78d32233
Matthew Pugh <pughmds@cyram.org>
2024-03-28 11:26:39 +0000

    Fixed location sensor so it should work with s3 URLS

--- liiatools_pipeline/sensors/location_sensor.py
+++ liiatools_pipeline/sensors/location_sensor.py
@@ -1,10 +1,9 @@
 import os
 from hashlib import sha1
-from dagster import RunRequest, SkipReason, RunConfig, sensor
+from dagster import RunRequest, SkipReason, RunConfig, sensor, DefaultSensorStatus
 from fs import open_fs
 from fs.walk import Walker
 from liiatools_pipeline.jobs.ssda903 import ssda903_incoming
-from liiatools_pipeline.ops.common import FileConfig
 from decouple import config as env_config
 
 
@@ -35,7 +34,9 @@ def generate_run_key(folder_location, files):
         # Open the filesystem
         with open_fs(folder_location) as filesystem:
             # Get the last modified timestamp of the file
-            last_modified_time = filesystem.getinfo(file_path, namespaces=['details']).modified
+            last_modified_time = filesystem.getinfo(
+                file_path, namespaces=["details"]
+            ).modified
 
             # Update the hash object with the string representation of the timestamp
             hash_object.update(str(last_modified_time).encode())
@@ -44,9 +45,16 @@ def generate_run_key(folder_location, files):
     return hash_object.hexdigest()
 
 
-@sensor(job=ssda903_incoming, minimum_interval_seconds=300, description="Monitors Specified Location for 903 Files")
+@sensor(
+    job=ssda903_incoming,
+    minimum_interval_seconds=300,
+    description="Monitors Specified Location for 903 Files",
+    default_status=DefaultSensorStatus.RUNNING,
+)
 def location_sensor(context):
-    context.log.info("Opening folder location: {}".format(env_config("INCOMING_LOCATION")))
+    context.log.info(
+        "Opening folder location: {}".format(env_config("INCOMING_LOCATION"))
+    )
     folder_location = env_config("INCOMING_LOCATION")
     wildcards = env_config("903_WILDCARDS").split(",")
     directory_pointer = open_location(folder_location)
@@ -57,12 +65,11 @@ def location_sensor(context):
     context.log.info("Generating Run Key")
     files = []
     for filename in directory_contents:
-        #files.append(directory_pointer.getsyspath(filename))
         files.append(filename.lstrip("/"))
 
     if not files:
-        context.log.info("No new files found, skipping run")
-        yield SkipReason("No new files")
+        context.log.info("No files found, skipping run")
+        yield SkipReason("No files present")
     else:
         context.log.info("Differences found, executing run")
         yield RunRequest(
