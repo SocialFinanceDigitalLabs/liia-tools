8ed6d180d0b037c39cf964b61a2f40af62b099e5
Tambe Tabitha <tambe.tabitha@socialfinance.org.uk>
2024-04-10 12:18:40 +0100

    sufficiency transform ops and job

--- liiatools_pipeline/jobs/sufficiency_903.py
+++ liiatools_pipeline/jobs/sufficiency_903.py
@@ -1,7 +1,14 @@
 from dagster import job
-from liiatools_pipeline.ops.sufficiency903 import ons_area
+from liiatools_pipeline.ops.sufficiency903 import dim_tables, ons_area, looked_after_child, ofsted_provider, postcode, episode, ofsted_inspection
 
 
 @job
 def ssda903_sufficiency():
+    dim_tables()
     ons_area()
+    looked_after_child()
+    ofsted_provider()
+    postcode()
+    episode()
+    ofsted_inspection()
+
