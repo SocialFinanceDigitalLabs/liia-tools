877871ce3c0c868fba9bb727e99ce2915fbce0be
Patrick Troy <patrick.troy@socialfinance.org.uk>
2024-06-05 15:50:27 +0100

    update reformat csv

--- liiatools/reformat_csv.py
+++ liiatools/reformat_csv.py
@@ -1,6 +1,7 @@
 import click as click
 import pandas as pd
 
+import csv
 import os
 import click_log
 import logging
@@ -26,7 +27,31 @@ def reformat_csv():
 def process(input):
     for root, dirs, files in os.walk(input):
         for file in files:
+            new_data = []
             file_loc = f"{root}\\{file}"
             if file_loc.endswith(".csv"):
-                df = pd.read_csv(file_loc, sep=",", header=0)
-                df.to_csv(file_loc, index=False)
+                try:
+                    pd.read_csv(file_loc)
+                    pass
+                except pd.errors.ParserError:
+                    with open(file_loc, "r") as f:
+                        data = f.read().splitlines()
+                        headers_len = len(data[0].split(","))
+                        row_no = 0
+                        for row in data:
+                            row_no += 1
+                            split_row = row.split(",")
+                            split_row_len = len(split_row)
+                            if split_row_len == headers_len:
+                                new_data.append(split_row)
+                            elif split_row_len > headers_len:
+                                extra_columns = split_row[headers_len:]
+                                if not any(extra_columns):
+                                    new_data.append(split_row[:headers_len-split_row_len])
+                                else:
+                                    raise Exception(f"Non blank extra columns in {file_loc} on row: {row_no}")
+
+            if new_data:
+                with open(file_loc[:-4] + "_test.csv", "w", newline="") as f:
+                    csvwriter = csv.writer(f, delimiter=',')
+                    csvwriter.writerows(new_data)
